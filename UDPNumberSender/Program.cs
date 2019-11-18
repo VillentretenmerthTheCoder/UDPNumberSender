0e45c2e723b553843e36a237ed3c32a21ba29727
Bartosz <DonMarchewa@gmail.com>
2019-11-18 10:44:33 +0100

    broadcasting added

--- UDPNumberSender/Program.cs
+++ UDPNumberSender/Program.cs
@@ -14,7 +14,7 @@ namespace UDPNumberSender
             IPAddress ip = IPAddress.Parse("127.0.0.1");
             UdpClient udpClient = new UdpClient("127.0.0.1", 9999);
 
-            IPEndPoint RemoteIpEndPoint = new IPEndPoint(ip, 9999); 
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Broadcast, 9999); 
 
 
             while (true)
@@ -22,7 +22,7 @@ namespace UDPNumberSender
             {
                 Byte[] sendBytes = Encoding.ASCII.GetBytes(" Pollution sensor v.1.0.\r\n Location: Maglegårdsvej 2 Time: 04-Oct-17/12:34:42 PM\r\n CO: 0.38 r \r\n NOx: 140.30 \r\n Particle level: Medium \r\n");
 
-                udpClient.Send(sendBytes, sendBytes.Length); 
+                udpClient.Send(sendBytes, sendBytes.Length, RemoteIpEndPoint); 
                 Thread.Sleep(4000);
             }
         }
